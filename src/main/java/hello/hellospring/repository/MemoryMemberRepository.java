89c75ee08047f1c2c9d036e2b4b29b5a06a8e2b9
chsubin <sbchoe21@gmail.com>
2023-05-18 02:17:36 +0900

    2023-05-18 스프링부트입문

---
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,42 @@
+package hello.hellospring.repository;
+
+import org.springframework.boot.autoconfigure.hateoas.HateoasProperties;
+
+import java.util.*;
+
+import hello.hellospring.domain.Member;
+
+public class MemoryMemberRepository implements MemberRepository {
+
+    private static Map<Long, Member> store = new HashMap<>();  //저장하기 위함
+    private static long sequence = 0L; //키값 생성
+
+
+    @Override
+    public hello.hellospring.domain.Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(),member);
+        return member;
+    }
+
+    @Override
+    public Optional<hello.hellospring.domain.Member> findById(Long id) {
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<hello.hellospring.domain.Member> findByName(String name) {
+        return store.values().stream()
+                .filter(member->member.getName().equals(name)) //맞는 이름이 나오면 반환시켜준다.
+                .findAny();
+    }
+
+    @Override
+    public List<hello.hellospring.domain.Member> findAll() {
+        return new ArrayList<>(store.values());
+    }
+    public void clearStore(){
+        store.clear();
+    }
+
+}
